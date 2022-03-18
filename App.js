ff0200cc9857cf59275d65f969a230a0eed5f01f
Roberto Hernandez <rchernandezd@gmail.com>
2022-03-17 21:07:52 -0300

    Cambio

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StatusBar } from 'expo-status-bar';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola, Coder2</Text>
+      <Text>Hola, Coder</Text>
       <StatusBar style="auto" />
     </View>
   );
